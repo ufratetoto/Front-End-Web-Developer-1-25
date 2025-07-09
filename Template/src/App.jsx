a50eadf7d607722e15153fc64e2271c52992b9b4
Giuseppe <giuseppe.campo@gmail.com>
2025-07-09 23:32:25 +0200

    Ultime modifiche

--- Template/src/App.jsx
+++ Template/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import Titolo from './componenti/Titolo.jsx'
 function App() {
 
   return <>
     <h1>React + Bootstrap Template</h1>
+    <Titolo>Eccomi, sono un titolo</Titolo>
     <p>Inserire qui il proprio codice</p>
   </>
 }
